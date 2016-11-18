16b273988667ab77be5644cbb9c42f5b67989db8
Hector Orellana <pulga.p.29@gmail.com>
2016-11-17 21:19:37 -0300

    added create_process function

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 enum CpuState {
   ReadyQueue1,
@@ -16,8 +17,22 @@ struct Process {
     int tiempo[3];
 };
 
+struct Process create_process(int id, char *name) {
+  struct Process p;
+  p.process_id = id;
+  p.estado = ReadyQueue1;
+  p.tiempo[0] = 0;
+  p.tiempo[1] = 0;
+  p.tiempo[2] = 0;
+  strncpy(p.nombre, name, strlen(name));
+
+  return p;
+}
+
 int main(void)
 {
-    printf("First commit!\n");
-    return (0);
+  struct Process p = create_process(1, "foo");
+
+  printf("pid: %d\nnombre: %s\nestado: %d\n", p.process_id, p.nombre, p.estado);
+  return (0);
 }

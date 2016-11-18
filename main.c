513171a7b19ac32aa943b8a92b573c4387337eea
Hector Orellana <pulga.p.29@gmail.com>
2016-11-17 21:28:50 -0300

    english motherfucker

--- main.c
+++ main.c
@@ -1,35 +1,43 @@
+#include <time.h>
 #include <stdio.h>
 #include <string.h>
 
 struct MeasuredTime {
-  time_t inicial;
+  time_t initial;
   time_t final;
-}
+};
 
-  enum CpuState {
-    ReadyQueue1,
-    CPU1,
-    IO,
-    ReadyQueue2,
-    CPU2,
-    Done,
-  };
+enum CpuState {
+  ReadyQueue1,
+  CPU1,
+  IO,
+  ReadyQueue2,
+  CPU2,
+  Done,
+};
 
 struct Process {
   int id;
-  char nombre[100];
-  int estado;
-  struct MeasuredTime[3];
+  char name[100];
+  int state;
+  struct MeasuredTime times[3];
 };
+
+struct MeasuredTime create_measured_time() {
+  struct MeasuredTime mt;
+  mt.initial = 0;
+  mt.final = 0;
+  return mt;
+}
 
 struct Process create_process(int id, char *name) {
   struct Process p;
   p.id = id;
-  p.estado = ReadyQueue1;
-  p.tiempo[0] = 0;
-  p.tiempo[1] = 0;
-  p.tiempo[2] = 0;
-  strncpy(p.nombre, name, strlen(name));
+  p.state = ReadyQueue1;
+  p.times[0] = create_measured_time();
+  p.times[1] = create_measured_time();
+  p.times[2] = create_measured_time();
+  strncpy(p.name, name, strlen(name));
 
   return p;
 }
@@ -38,6 +46,6 @@ int main(void)
 {
   struct Process p = create_process(1, "foo");
 
-  printf("pid: %d\nnombre: %s\nestado: %d\n", p.id, p.nombre, p.estado);
+  printf("pid: %d\nnombre: %s\nestado: %d\n", p.id, p.name, p.state);
   return (0);
 }

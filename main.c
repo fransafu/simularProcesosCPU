4d09c0d2e3f31e974e7898587c292e21a22fd92f
Hector Orellana <pulga.p.29@gmail.com>
2016-11-17 21:07:24 -0300

    added cpu states

--- main.c
+++ main.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 
+enum CpuState {
+  ReadyQueue1,
+  CPU1,
+  IO,
+  ReadyQueue2,
+  CPU2,
+  Done,
+};
+
 struct Process {
     int process_id;
     char nombre[100];
     int estado;
     int tiempo[3];
-}
+};
 
 int main(void)
 {
